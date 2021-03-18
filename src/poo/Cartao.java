0e23af14b4067783045db060f85284bd7e90723b
bmcandeias <50877834+bmcandeias@users.noreply.github.com>
2021-03-18 18:17:41 +0000

    Informações de decumentação do documento

--- src/poo/Cartao.java
+++ src/poo/Cartao.java
@@ -1,17 +1,30 @@
 package poo;
 
 public class Cartao {
-	
+	//Atributos 
+	/**
+	 * cria as variaveis privadas
+	 */
 	private String nomeFuncionario;
 	private String servico;
 	private String cargo;
 	
 	// Construtores 
+	/**
+	 * 
+	 * @param nomeFuncionario
+	 * escolhe o nome do funcionario
+	 */
 	public Cartao(String nomeFuncionario) {
 		super();
 		this.nomeFuncionario = nomeFuncionario;
 	}
-
+	/**
+	 * Associa informação dada para cada variavel
+	 * @param nomeFuncionario
+	 * @param servico
+	 * @param cargo
+	 */
 	public Cartao(String nomeFuncionario, String servico, String cargo) {
 		super();
 		this.nomeFuncionario = nomeFuncionario;
@@ -20,7 +33,10 @@ public class Cartao {
 	}
 
 	// Acessores
-	
+	/**
+	 * faz return do nome do funcionario e do servico e cargo
+	 * @return
+	 */
 	public String getNomeFuncionario() {
 		return nomeFuncionario;
 	}
@@ -34,21 +50,32 @@ public class Cartao {
 	}
 	
 	// Métodos
+	/**
+	 * retira os servilco
+	 */
 	public void retirarServico() {
 		// servico = "";
 		setServico("");
 	}
-	
+	/**
+	 * atribui o servico novo
+	 * @param novoServico
+	 */
 	public void atribuirNovoServico( String novoServico ) {
 		//servico = novoServico;
 		setServico( novoServico );
 	}
-	
+	/**
+	 * atribui um novo cargo
+	 * @param novoCargo
+	 */
 	public void promoverNovoCargo( String novoCargo ) {
 		//cargo = novoCargo;
 		setCargo(novoCargo);
 	}
-	
+	/**
+	 * despedir ele apaga apaga um funcionario com os servico e a carga
+	 */
 	public void despedirFuncionario() {
 		retirarServico();
 		setCargo("");
